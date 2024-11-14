a004abd178dda1a445fc3ee9d992eabeae21f308
Melissa Garcia <melissag@ensemble.com>
2024-11-14 14:36:38 -0800

    set path prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@
  */
 
 module.exports = {
-  pathPrefix: process.env.PATH_PREFIX || '/dev-site-documentation-template/',
+  pathPrefix: process.env.PATH_PREFIX || '/acrobat-sign/developer-guide/',
   siteMetadata: {
     versions: [
       {
